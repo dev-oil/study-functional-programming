03a1e514b827606b8f60fb47e8268c84ea55859b
devoil <devdevoildev@gmail.com>
2025-09-18 20:29:00 +0900

    feat: [함수형 자바스크립트 1] 함수형 기본 - 일급함수

--- functional-javascript-01/01. 함수형 자바스크립트 기본기/1.js
+++ functional-javascript-01/01. 함수형 자바스크립트 기본기/1.js
@@ -13,3 +13,22 @@ const a = 10;
 const add10 = (a) => a + 10;
 const r = add10(a);
 log(r);
+
+// # 일급 함수
+// - 함수를 값으로 다룰 수 있다.
+// - 조합성과 추상화의 도구
+
+const add5 = (a) => a + 5;
+log(add5); // a => a + 5
+log(add5(5)); // 10
+
+// 함수 실행 시 함수를 리턴
+const f1 = () => () => 1;
+log(f1()); // () => 1
+
+const f2 = f1();
+log(f2); // () => 1
+log(f2()); // 1
+
+const f3 = () => 1;
+console.log(f3); // () => 1
